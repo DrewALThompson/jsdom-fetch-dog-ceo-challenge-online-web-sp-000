0f6b4a19574e8398da20214aa58a434bd0469cd9
Drew Thompson <drewallynthompson@gmail.com>
2021-01-06 07:32:43 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 console.log('%c HI', 'color: firebrick')
 
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 dogContainer = document.getElementById('dog-image-container');
-
 dogImage = document.createElement('img')
 
 function toImg(){
