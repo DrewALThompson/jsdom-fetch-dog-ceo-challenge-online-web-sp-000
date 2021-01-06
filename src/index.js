890305da3b3862673354ae31530f56afa6350d48
Drew Thompson <drewallynthompson@gmail.com>
2021-01-06 07:47:32 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 dogContainer = document.getElementById('dog-image-container');
-dogImage = document.createElement('img')
+dogImage = document.createElement('img');
 
 
 function fetchDogs(){
   fetch(imgUrl)
   .then(res => res.json())
-  .then(json => )
+  .then(json => dogPics(json));
+}
+
+function dogPics(){
+  
 }
