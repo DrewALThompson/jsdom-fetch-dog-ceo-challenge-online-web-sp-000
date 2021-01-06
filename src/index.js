e06084044792870393d2bfdeac5700f5af289661
Drew Thompson <drewallynthompson@gmail.com>
2021-01-06 07:59:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ function dogPics(json){
   json.message.forEach(pic => {
     dogImage = document.createElement('img');
     dogImage.src = pic;
-    dogContainer.appendChild(dogImage);
+    dogContainer.append(dogImage);
   });
 }
